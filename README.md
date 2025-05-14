a1a930c75b2dfe796e52b6fc6f317e4c5fa939d2
m8test <contact@m8test.com>
2025-05-14 17:24:45 +0800

    feat: 初始化项目

---
+++ README.md
@@ -0,0 +1,6 @@
+# [M8Test](https://dev-docs.m8test.com) gradle 模板
+
+> 如有任何疑问请添加我们的官方q群: [749248182](https://qm.qq.com/q/jVVADJCAI8) 或者 qq
+> 频道 [m8testofficial](https://pd.qq.com/s/8pidhywcy)
+
+本项目中包含了所有已支持的语言的gradle模板, 请查看对应语言分支获取源码。
---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/apache-snapshots")
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenLocal()
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/apache-snapshots")
+        mavenCentral()
+        google()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "m8test-gradle-templates"
