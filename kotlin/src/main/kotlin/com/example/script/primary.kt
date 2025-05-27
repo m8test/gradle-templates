71537db685faa9670a4325eb52f6f0adf6e1980e
m8test <contact@m8test.com>
2025-05-27 11:23:45 +0800

    feat: 增加了UI相关代码

--- kotlin/src/main/kotlin/com/example/script/primary.kt
+++ kotlin/src/main/kotlin/com/example/script/primary.kt
@@ -1,10 +1,33 @@
 package com.example.script
 
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
 import com.example.tool.ToolB
-import com.m8test.script.GlobalVariables._console
+import com.m8test.script.GlobalVariables.*
 
 fun run() {
     // 这里写代码
-    var tool: ToolB = ToolB()
+    val tool = ToolB()
     tool.methodB(_console)
+    _androidView.create(true) {
+        val button = Button(context)
+        button.text = "Hello"
+        var num = 0
+        val lp = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT)
+        // 居中显示按钮
+        lp.gravity = Gravity.CENTER
+        // 这里不能使用 lambda, 否则会崩溃
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                v as Button
+                v.text = "Hello${num++}"
+            }
+        })
+        // 添加按钮到界面
+        addView(button, lp)
+    }
+    // 显示界面
+    _activity.start()
 }
