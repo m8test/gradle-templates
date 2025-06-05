417c72fb50dfc63e170a62aa413d8a3acaa1236b
m8test <contact@m8test.com>
2025-06-05 21:42:38 +0800

    feat: 增加了访问jvm的示例代码

--- javascript/src/com/example/script/primary.js
+++ javascript/src/com/example/script/primary.js
@@ -10,16 +10,18 @@ $androidView.create(true, (frameLayout) => {
     // 创建按钮
     let button = new Packages.android.widget.Button(frameLayout.getContext())
     button.setText("测试按钮")
-    button.setOnClickListener(function (view) {
-        num++
-        button.setText("测试按钮" + num)
+    button.setOnClickListener({
+        onClick: function (view) {
+            num++
+            button.setText("测试按钮" + num)
+        }
     })
     let LP = Packages.android.widget.FrameLayout.LayoutParams
     // 通过 `.` 直接访问java静态属性
     let layoutParams = new LP(LP.WRAP_CONTENT, LP.WRAP_CONTENT)
     // 设置按钮居中显示
     layoutParams.gravity = Packages.android.view.Gravity.CENTER
-    // 添加按钮到界面中
+    // @ts-ignore 添加按钮到界面中
     frameLayout.addView(button, layoutParams)
 })
 // 启动 android 活动用于展示脚本UI
