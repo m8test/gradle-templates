8f76e041f2f27d8040671618404a0048101977e4
m8test <contact@m8test.com>
2025-07-07 17:03:01 +0800

    build: 更新m8test版本到0.1.10

--- php/build.gradle.kts
+++ php/build.gradle.kts
@@ -29,7 +29,7 @@ m8testPhp {
         // 打包apk时的运行时模板apk配置, 打包apk时会将脚本项目打包到该模板apk中
         runtimeConfig {
             // 模板apk的版本
-            versionName = "0.1.8"
+            versionName = "0.1.10"
             // 模板apk下载地址
             downloadUrl =
                 "https://github.com/m8test/runtime-release/releases/download/${versionName}/com.m8test.app.runtime-release_${versionName}.apk"
@@ -39,7 +39,7 @@ m8testPhp {
             // 开发工具的包名, 使用默认就好
             packageName = "com.m8test.app.developmentkit"
             // 开发工具版本
-            versionName = "0.1.8"
+            versionName = "0.1.10"
             // 开发工具下载地址
             downloadUrl =
                 "https://github.com/m8test/development-kit-release/releases/download/$versionName/com.m8test.app.developmentkit-release_$versionName.apk"
