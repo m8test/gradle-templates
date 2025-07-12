c6792f9186c40a8db960c9b1a8e80a31eb46873a
m8test <contact@m8test.com>
2025-07-12 08:40:56 +0800

    build: 更新scrcpy版本到3.3.1

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 }
 // 在gradle.properties中定义的变量, 用于指定m8test版本
 val m8testVersion: String by project
+// 在gradle.properties中定义的变量, 用于指定scrcpy版本
+val scrcpyVersion: String by project
 // m8testJava 闭包用于配置 m8test java 项目
 m8testJava {
     // debugger 闭包用于配置调试器(安卓设备)的信息
@@ -72,7 +74,7 @@ m8testJava {
             // adb自动化组件
             name = "YumiMiyamotoScrcpy"
             // 组件版本
-            version = "3.2.0"
+            version = scrcpyVersion
             // 组件下载地址
             url =
                 "https://github.com/YumiMiyamoto/scrcpy-release/releases/download/Scrcpy-$version/com.m8test.scrcpy-release_$version.apk"
