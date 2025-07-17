9a201c5e20fd02d42df8f8ffb2946223f9390d2d
m8test <contact@m8test.com>
2025-07-17 18:16:10 +0800

    build: 增加了assets相关配置

--- ruby/build.gradle.kts
+++ ruby/build.gradle.kts
@@ -52,6 +52,13 @@ m8testRuby {
     }
     // 脚本项目配置闭包
     projectConfig {
+        // 相对于res目录的资源文件,会打包到apk的assets目录下
+        // 这个文件表示apk启动时自动执行的脚本, 只能是true或者false
+        addAsset("assets/auto_start_spa.txt")
+        // 这个文件表示脚本项目官网,可以是一个网址
+        addAsset("assets/official_website.txt")
+        // 这个文件表示脚本项目版权信息
+        addAsset("assets/copyright.txt")
         // 脚本项目入口文件, 这里是相对于src目录的路径
         entry = "com/example/script/primary.rb"
         // 脚本项目包名, 和java包名规则一样
