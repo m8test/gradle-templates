1aa95d1299cb67c53623e74b25f5db281ed20275
m8test <contact@m8test.com>
2025-09-19 15:30:05 +0800

    build: 升级m8test版本到0.1.23以及m8test gradle版本到0.1.20

--- python/build.gradle.kts
+++ python/build.gradle.kts
@@ -116,10 +116,9 @@ m8testPython {
     tools {
         // 测试文件的配置
         test {
-            // 需要测试的文件路径，相对于模块根目录
-            localPath = "build/test/test.py"
-            // 测试文件在设备中的路径
-            remotePath = "/sdcard/M8Test/test.py"
+            // key 为需要测试的文件路径，相对于模块根目录, value 为测试文件在设备中的路径
+            paths["build/test/test.py"] = "/sdcard/M8Test/test.py"
+            paths["build/test/test1.py"] = "/sdcard/M8Test/test1.py"
         }
         // 端口转发，可以将安卓设备端的端口映射到电脑端
         portForwarding {
