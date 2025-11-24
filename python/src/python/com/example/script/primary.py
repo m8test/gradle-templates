da82f613ac5e5e755ce82f550744b97e0e6e4ddc
m8test <contact@m8test.com>
2025-11-24 11:25:53 +0800

    refactor: 将示例代码从原生ui改成compose ui

--- python/src/python/com/example/script/primary.py
+++ python/src/python/com/example/script/primary.py
@@ -1,36 +1,62 @@
-# from 引入时必须使用相对于src目录的路径
-from m8test_java.android.view.Gravity import Gravity
-from m8test_java.android.widget.Button import Button
-from m8test_java.android.widget.FrameLayout import FrameLayout
 # 必须使用 `from m8test_java.com.m8test.script.GlobalVariables import xxx` xxx是全局变量名
 from m8test_java.com.m8test.script.GlobalVariables import _activity
-from m8test_java.com.m8test.script.GlobalVariables import _androidView
+from m8test_java.com.m8test.script.GlobalVariables import _composeView
 from m8test_java.com.m8test.script.GlobalVariables import _console
-from m8test_java.com.m8test.script.GlobalVariables import _reflectors
-from m8test_java.com.m8test.script.GlobalVariables import runOnUiThread
+
+# from 引入时必须使用相对于src目录的路径
 from python.com.example.tool.ToolB import ToolB
 
 toolB = ToolB()
 toolB.methodB(_console)
 
-
-# 用于构建界面的函数
-def fn(frame_layout: FrameLayout):
-    button = Button(frame_layout.getContext())
-    button.setText("Hello")
-    # 添加按钮点击事件, 点击后设置按钮文本为Hi
-    button.setOnClickListener(lambda _: button.setText("Hi"))
-    # 如果不能通过`.`访问静态属性, 那么可以反射获取
-    wrap_content = (_reflectors.reflect(FrameLayout.LayoutParams)
-                    .getField(None, lambda selector: selector.setName("WRAP_CONTENT")))
-    layout_params = FrameLayout.LayoutParams(wrap_content, wrap_content)
-    # 居中显示按钮, 访问非内部类静态属性直接通过`.`调用
-    layout_params.gravity = Gravity.CENTER
-    # 添加视图时必须在安卓UI线程
-    runOnUiThread(lambda: frame_layout.addView(button, layout_params))
-
-
-# 创建需要显示的界面
-_androidView.create(False, fn)
+# 使用 Compose UI 创建界面
+_composeView.create(
+    # 创建一个垂直方向的布局
+    lambda slot: slot.Column(
+        lambda column: (
+            # 设置 Column 修饰符, 使其填满整个屏幕
+            column.setModifier(lambda modifier: modifier.fillMaxSize(1.0)),
+            # 设置 Column 内部子元素的对齐方式为水平居中对齐
+            column.setHorizontalAlignment(
+                lambda alignments: alignments.getCenterHorizontally()
+            ),
+            # 设置 Column 内部子元素在垂直方向的排列方式为居中排列
+            column.setVerticalArrangement(
+                lambda arrangements: arrangements.getCenter()
+            ),
+            # 设置 Column 的内容
+            column.setContent(
+                lambda columnSlot: (
+                    # 1. 使用 mutableStateOf 创建可变状态
+                    (state := columnSlot.mutableStateOf(0)),
+                    # 在 Text 组件中显示点击次数
+                    columnSlot.Text(
+                        lambda text: (
+                            # 2. 使用 trackSingleState 追踪状态变化, 如果状态值改变的话, 该 Composable 会重新组合
+                            text.trackSingleState(state),
+                            # 3. 使用 state.getValue() 获取状态值
+                            text.setText("点击次数" + str(state.getValue())),
+                        )
+                    ),
+                    # 创建一个按钮，点击按钮时修改状态值
+                    columnSlot.TextButton(
+                        lambda textButton: (
+                            # 设置按钮内容显示一个简单的文本
+                            textButton.setContent(
+                                lambda buttonSlot: buttonSlot.Text(
+                                    lambda text: text.setText("点击")
+                                )
+                            ),
+                            textButton.setOnClick(
+                                # 4. 使用 state.setValue(newValue) 修改状态值
+                                lambda: state.setValue(state.getValue() + 1)
+                            ),
+                        )
+                    ),
+                )
+            ),
+        )
+    )
+)
 # 启动ui界面
 _activity.start()
