e26af143dc5b3115eb641c454503cb9452588a60
m8test <contact@m8test.com>
2025-12-09 21:37:10 +0800

    build: 更新m8test版本到0.1.35以及m8test gradle 版本到0.1.24

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -45,6 +45,8 @@ m8testJava {
             // 开发工具下载地址
             downloadUrl =
                 "https://github.com/m8test/development-kit-release/releases/download/$versionName/com.m8test.app.developmentkit-release_$versionName.apk"
+            // api文档根目录
+            docsRoot = "https://sdk-docs.m8test.com/sdk"
         }
     }
     // 脚本项目配置闭包
@@ -75,6 +77,8 @@ m8testJava {
             // 组件下载地址
             url =
                 "https://github.com/YumiMiyamoto/accessibility-release/releases/download/Accessibility-$version/com.m8test.accessibility-release_$version.apk"
+            // api文档根目录
+            docsRoot = "https://yumimiyamoto.github.io/accessibility-release/app"
         }
         // 在脚本项目中引用 M8Test 组件
         requireComponent {
@@ -85,6 +89,8 @@ m8testJava {
             // 组件下载地址
             url =
                 "https://github.com/YumiMiyamoto/ocr-release/releases/download/ocr-$version/com.m8test.ocr-release_$version.apk"
+            // api文档根目录
+            docsRoot = "https://yumimiyamoto.github.io/ocr-release/ocr"
         }
         // 在脚本项目中引用 M8Test 组件
         requireComponent {
@@ -95,6 +101,8 @@ m8testJava {
             // 组件下载地址
             url =
                 "https://github.com/YumiMiyamoto/opencv-release/releases/download/opencv-$version/com.m8test.image-release_$version.apk"
+            // api文档根目录
+            docsRoot = "https://yumimiyamoto.github.io/opencv-release/image"
         }
     }
     // 工具相关配置
