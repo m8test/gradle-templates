b470efb23818d6a693c85870bf36be180da267cd
m8test <contact@m8test.com>
2026-03-16 20:34:43 +0800

    build: 去除buildSrc模块、升级m8test版本到0.1.42以及m8test gradle版本到0.1.28

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,32 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.kotlin) apply false
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://jitpack.io")
+        // jcenter()
+        google()
+        val m8testVersion: String by project
+        maven {
+            url = uri("https://raw.githubusercontent.com/m8test/Maven/refs/heads/${m8testVersion}/")
+        }
+        val properties = java.util.Properties().apply {
+            java.io.FileInputStream(File(rootDir, "gradle.properties")).use { load(it) }
+        }
+        maven {
+            url =
+                uri(
+                    "https://raw.githubusercontent.com/m8test/development-environment/refs/heads/v${
+                        properties.getProperty("m8testGradleVersion")
+                    }/"
+                )
+        }
+    }
+    dependencies {
+        val m8testGradleVersion: String by project
+        classpath("com.m8test:gradle-plugin:${m8testGradleVersion}")
+    }
 }
-
 //apply(plugin = libs.plugins.m8test.groovy.get().pluginId)
 //apply(plugin = libs.plugins.m8test.java.get().pluginId)
 //apply(plugin = libs.plugins.m8test.javascript.get().pluginId)
