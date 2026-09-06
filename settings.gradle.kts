d856fa26198ea3516aa78932c4d35baeccd7dd55
kudoyukiko <jackhai66@proton.me>
2026-09-06 04:22:37 -0400

    build(php): 同步 Gradle 与脚本任务配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,9 @@ dependencyResolutionManagement {
         google()
         maven("https://jitpack.io")
     }
+    versionCatalogs {
+        create("m8test") { from("com.m8test:version-catalog:$m8testVersion") }
+    }
 }
 
 rootProject.name = "m8test-gradle-templates"
