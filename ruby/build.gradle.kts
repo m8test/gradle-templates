bc348afd70b8693d676478937528c7b11b5a7f12
kudoyukiko <jackhai66@proton.me>
2026-09-06 06:35:31 -0400

    fix(template): 配置组件 Extension ID

--- ruby/build.gradle.kts
+++ ruby/build.gradle.kts
@@ -93,6 +93,7 @@ m8testRuby {
         requireComponent {
             // 无障碍组件
             name = "YumiMiyamotoAccessibility"
+            properties["com.m8test.extension.id"] = "com.m8test.extension.component.accessibility"
             // 组件版本
             version = accessibilityVersion
             // 组件下载地址
@@ -105,6 +106,7 @@ m8testRuby {
         requireComponent {
             // ocr组件
             name = "YumiMiyamotoOcr"
+            properties["com.m8test.extension.id"] = "com.m8test.extension.component.ocr"
             // 组件版本
             version = ocrVersion
             // 组件下载地址
@@ -117,6 +119,7 @@ m8testRuby {
         requireComponent {
             // opencv 图色组件
             name = "YumiMiyamotoOpencv"
+            properties["com.m8test.extension.id"] = "com.m8test.extension.component.image"
             // 组件版本
             version = opencvVersion
             // 组件下载地址
