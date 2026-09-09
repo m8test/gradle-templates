cb1433a3e469977fe9b3eb42fdfd86ebfc3ec02e
kudoyukiko <jackhai66@proton.me>
2026-09-09 10:51:38 -0400

    test(java): 增加设备脚本时序与事件标记

--- java/src/main/java/com/example/script/side1/side1.java
+++ java/src/main/java/com/example/script/side1/side1.java
@@ -4,15 +4,22 @@ import static com.m8test.script.GlobalVariables.*;
 
 class side1 {
     public static void run() {
+        $logger.info("SIDE1_STARTED");
         var subscriber = $events.getSubscriber();
         subscriber.subscribe(scopes -> scopes.getScript(), "subscription-channel", event -> {
-            $logger.info("收到本地事件 " + event.getPayload().getStringOrNull("data") + " "
+            $logger.info("SIDE1_LOCAL_EVENT " + event.getPayload().getStringOrNull("data") + " "
                 + event.getTimeMillis() + " " + event.getChannel());
         });
         subscriber.subscribe(scopes -> scopes.getApp(), "subscription-channel", event -> {
-            $logger.info("收到全局事件 " + event.getPayload().getStringOrNull("data") + " "
+            $logger.info("SIDE1_GLOBAL_EVENT " + event.getPayload().getStringOrNull("data") + " "
                 + event.getTimeMillis() + " " + event.getChannel());
         });
+        $logger.info("SIDE1_SUBSCRIBED");
+        try {
+            java.lang.Thread.sleep(1500L);
+        } catch (java.lang.InterruptedException ignored) {
+            // The script is being stopped.
+        }
     }
 }
 //-m8test-remove side1.run();
--- java/src/main/java/com/example/script/side2/side2.java
+++ java/src/main/java/com/example/script/side2/side2.java
@@ -4,6 +4,7 @@ import static com.m8test.script.GlobalVariables.*;
 
 class side2 {
     public static void run() {
+        $logger.info("SIDE2_STARTED");
         var publisher = $events.getPublisher();
         publisher.publish(scopes -> scopes.getScript(), "subscription-channel", payload -> {
             payload.putString("data", "本地事件");
@@ -11,6 +12,7 @@ class side2 {
         publisher.publish(scopes -> scopes.getApp(), "subscription-channel", payload -> {
             payload.putString("data", "全局事件");
         });
+        $logger.info("SIDE2_PUBLISHED");
     }
 }
 //-m8test-remove side2.run();
